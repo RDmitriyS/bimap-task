4b63bf1c6c71625e50e8b326773f0967912c0999
Zakhar Koval <ellesterate@gmail.com>
2020-11-24 22:58:17 +0300

    Foo bar

--- build.sh
+++ build.sh
@@ -2,5 +2,11 @@
 
 mkdir -p cmake-build-$1
 rm -rf cmake-build-$1/*
-cmake -DCMAKE_BUILD_TYPE=$1 -S . -B cmake-build-$1
+
+if [ "$1" = "Asan" ]; then
+    cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -fsanitize=address,undefined -fno-omit-frame-pointer" -S . -B cmake-build-$1
+else
+    cmake -DCMAKE_BUILD_TYPE=$1 -S . -B cmake-build-$1
+fi
+
 cmake --build cmake-build-$1
